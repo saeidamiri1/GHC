5c407cfdce1d625642092c505b9f0f567b72f0b2
Saeid Amiri <saeid.amiri1@gmail.com>
2017-05-24 23:23:17 -0500

    Update SHC.R

--- SHC.R
+++ SHC.R
@@ -1,8 +1,9 @@
-############# 27-OCT-2014
+############# 24-May-2017
 ############# It is just a trial version of codes.  
-############# samiri2@unl.edu 
+############# saeid.amiri1@gmail.com
 ###################################
 ################################# Ensemble    
+
 SHC<-function (x,kn0,B=200){
 	x<-cbind(x,rep(0,nrow(x)))
 Len<-dim(x)
@@ -11,16 +12,86 @@ b<-1
 # it is 4
 knmin<-2;knmax<-min(25,floor(dim(x)[1]/5)-2)
 kn<-sample(c(knmin,knmax),1)
+#dd<-Hub2MQ(x,kn)
+#RE<-dd[,Len[2]]
+
+
+
+
+RHub2MQ<-function(x,kn,knmin,knmax){
+kn<-sample(c(knmin:knmax),1)
 dd<-Hub2MQ(x,kn)
-RE<-dd[,Len[2]]
-while(b<B){
-  kn<-sample(c(knmin:knmax),1)
-  dd<-Hub2MQ(x,kn)
-  RE<-rbind(RE,dd[,Len[2]])    
-  b<-b+1
+Len<-dim(x)
+return(dd[,Len[2]])    
 }
 
-REDIST<-as.dist(distancematrixH(RE))
+distancematrix0<-function(data){
+  data<-reorderf(data)
+  Len<-dim(data)
+  nk<-as.integer(names(table(data[,Len[2]])))
+  ss<-length(unique(data[,Len[2]]))
+  dismat<- matrix(NA,ncol=ss,ss)#array(NA, dim=c(1,Len[2]-1,ss,ss))
+  for(i in 1:ss){
+    if (i==ss) break
+    for(j in ((i+1):ss)){
+      ij0<-data[,Len[2]]==i
+      ij1<-data[,Len[2]]==j
+      dismat[i,j]<-(distwo(data[ij0,-Len[2]],data[ij1,-Len[2]]))
+    }
+  }
+  t(dismat)
+}
+
+reorderf<-function(data){
+  Len<-dim(data)
+  nk<-as.integer(names(table(data[,Len[2]])))
+  h<-0
+  data0<-cbind(data,NA)
+  for(i in nk){
+    ij<-data[,Len[2]]==i
+    h<-h+1
+    data0[ij,Len[2]+1]<-h
+  }
+  return(data0[,-Len[2]])
+}
+
+
+distwo<-function(data1,data2){
+  d1<-dim(data1)
+  if(is.null(d1)) {data1<-t(as.matrix(data1));d1<-dim(data1)}
+  d2<-dim(data2)
+  if(is.null(d2)) {data2<-t(as.matrix(data2));d2<-dim(data2)}
+  di0<-NULL
+  ff<-1
+  for(i in 1:d1[1]){
+    for(j in 1:d2[1]){
+      di0[ff]<-mean((data1[i,]-data2[j,])^2)
+      ff<-ff+1
+    }
+  }
+  quantile(di0,probs=.2)
+}
+
+
+
+cl <- makeCluster(detectCores()-1) # create a cluster with 2 cores
+registerDoParallel(cl) # register the cluster
+ens = foreach(i = 1:200, 
+              .combine = "rbind", .export=c("Hub2MQ","distancematrix0","reorderf","distwo")) %dopar% {
+                fit1 <- RHub2MQ(x,kn,knmin,knmax)
+                fit1
+              }
+stopCluster(cl) 
+
+
+#while(b<B){
+#  kn<-sample(c(knmin:knmax),1)
+#  dd<-Hub2MQ(x,kn)
+#  RE<-rbind(RE,dd[,Len[2]])    
+#  b<-b+1
+#}
+
+REDIST<-as.dist(distancematrixH(ens))
 REDISTT<-as.matrix(REDIST)
 
 hc <- hclust(REDIST,method = "single")
@@ -136,13 +207,80 @@ EK<-function (x,B=200){
 kn<-sample(c(knmin,knmax),1)
 dd<-Hub2MQ(x,kn)
 RE<-dd[,Len[2]]
-while(b<B){
+
+
+
+#####
+####
+
+RHub2MQ<-function(x,kn,knmin,knmax){
   kn<-sample(c(knmin:knmax),1)
   dd<-Hub2MQ(x,kn)
-  RE<-rbind(RE,dd[,Len[2]])    
-  b<-b+1
+  Len<-dim(x)
+  return(dd[,Len[2]])    
 }
-  REDIST<-as.dist(distancematrixH(RE))
+
+distancematrix0<-function(data){
+  data<-reorderf(data)
+  Len<-dim(data)
+  nk<-as.integer(names(table(data[,Len[2]])))
+  ss<-length(unique(data[,Len[2]]))
+  dismat<- matrix(NA,ncol=ss,ss)#array(NA, dim=c(1,Len[2]-1,ss,ss))
+  for(i in 1:ss){
+    if (i==ss) break
+    for(j in ((i+1):ss)){
+      ij0<-data[,Len[2]]==i
+      ij1<-data[,Len[2]]==j
+      dismat[i,j]<-(distwo(data[ij0,-Len[2]],data[ij1,-Len[2]]))
+    }
+  }
+  t(dismat)
+}
+
+reorderf<-function(data){
+  Len<-dim(data)
+  nk<-as.integer(names(table(data[,Len[2]])))
+  h<-0
+  data0<-cbind(data,NA)
+  for(i in nk){
+    ij<-data[,Len[2]]==i
+    h<-h+1
+    data0[ij,Len[2]+1]<-h
+  }
+  return(data0[,-Len[2]])
+}
+
+
+distwo<-function(data1,data2){
+  d1<-dim(data1)
+  if(is.null(d1)) {data1<-t(as.matrix(data1));d1<-dim(data1)}
+  d2<-dim(data2)
+  if(is.null(d2)) {data2<-t(as.matrix(data2));d2<-dim(data2)}
+  di0<-NULL
+  ff<-1
+  for(i in 1:d1[1]){
+    for(j in 1:d2[1]){
+      di0[ff]<-mean((data1[i,]-data2[j,])^2)
+      ff<-ff+1
+    }
+  }
+  quantile(di0,probs=.2)
+}
+
+
+
+cl <- makeCluster(detectCores()-1) # create a cluster with 2 cores
+registerDoParallel(cl) # register the cluster
+ens = foreach(i = 1:B, 
+              .combine = "rbind", .export=c("Hub2MQ","distancematrix0","reorderf","distwo")) %dopar% {
+                fit1 <- RHub2MQ(x,kn,knmin,knmax)
+                fit1
+              }
+stopCluster(cl) 
+
+
+
+
   hclustM <- hclust(REDIST, method = "single")
   cutValue <- hclustM$height[which.max(diff(hclustM$height))]
   ee<-(cutree(hclustM, h = cutValue))
@@ -194,26 +332,24 @@ Hub2MQ<-function(x,kn){
 
 
 
-distancematrixHT<-function(data){
-  Len<-dim(data)
-  ss<-Len[2]
-  dismat<- matrix(NA,ncol=ss,ss)#array(NA, dim=c(1,Len[2]-1,ss,ss))
-  for(i in 1:ss){
-    for(j in 1:ss){
-      dismat[i,j]<-(distancem(data[,i],data[,j]))
-    }
-  }
-  t(dismat)
-}
+###distancematrixHT<-function(data){
+###  Len<-dim(data)
+###  ss<-Len[2]
+###  dismat<- matrix(NA,ncol=ss,ss)
+###  for(i in 1:ss){
+###    for(j in 1:ss){
+###      dismat[i,j]<-(distancem(data[,i],data[,j]))
+###    }
+###  }
+###  t(dismat)
+###}
 
 
 
 distancematrixH<-function(data){
-  #please use with more than two dimention
-  #data<-reorderf(data)
   Len<-dim(data)
   ss<-Len[2]
-  dismat<- matrix(NA,ncol=ss,ss)#array(NA, dim=c(1,Len[2]-1,ss,ss))
+  dismat<- matrix(NA,ncol=ss,ss)
   for(i in 1:ss){
     if (i==ss) break
     for(j in ((i+1):ss)){
@@ -266,7 +402,7 @@ distwo<-function(data1,data2){
   if(is.null(d1)) {data1<-t(as.matrix(data1));d1<-dim(data1)}
   d2<-dim(data2)
   if(is.null(d2)) {data2<-t(as.matrix(data2));d2<-dim(data2)}
-  di0<-NULL#matrix(NA,nrow=d1*d2,ncol=2)
+  di0<-NULL
   ff<-1
   for(i in 1:d1[1]){
     for(j in 1:d2[1]){
@@ -281,11 +417,7 @@ distwo<-function(data1,data2){
 distwoA2<-function(xlcT0,dat1,dat2){
   dat11<-unlist(dat1)
   dat22<-unlist(dat2)
-  #d1<-dim(data1)
-  #if(is.null(d1)) {data1<-t(as.matrix(data1));d1<-dim(data1)}
-  #d2<-dim(data2)
-  #if(is.null(d2)) {data2<-t(as.matrix(data2));d2<-dim(data2)}
-  di00<-NULL#matrix(NA,nrow=d1*d2,ncol=2)
+  di00<-NULL
   ff<-1
   for(ii1 in dat11){
     for(ii2 in dat22){
@@ -364,15 +496,11 @@ Xsub<-function(hclust0,K0){
   xcc<-CreatXCC(xx)
   xc<-NULL
   heigh1<-hclust0$heigh
-  
   inverse = function (f, lower = -100, upper = 100) {
     function (y) uniroot((function (x) f(x) - y), lower = lower, upper = upper)[1]
   }
   square_inverse = inverse(function (x) length(unique((cutree(hclust0,h=x)))), min(heigh1), max(heigh1))
-  
-  
   Km<-which.min((heigh1-square_inverse(K0)$root)^2)
-  
   for(l1 in (Km-1):(1)){
     if(l1 %in% xc) next
     if(xx[l1,1]<0&xx[l1,2]<0) {zh[l1]<-heigh1[l1]}
